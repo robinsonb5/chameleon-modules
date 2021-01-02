b79a6b9cdb2a2d874fa661ca3452d1abafbf6d35
Alastair M. Robinson <robinsonb5@gmail.com>
2021-01-02 23:45:29 +0000

    Initial commit

---
+++ chameleon_toplevel_e.vhd
@@ -0,0 +1,75 @@
+-- -----------------------------------------------------------------------
+--
+-- Turbo Chameleon
+--
+-- Toplevel entity for Turbo Chameleon 64
+--
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.ALL;
+
+library work;
+
+-- -----------------------------------------------------------------------
+
+entity chameleon_toplevel is
+	generic (
+		resetCycles: integer := 131071
+	);
+	port (
+-- Clocks
+		clk8 : in std_logic;
+		phi2_n : in std_logic;
+		dotclock_n : in std_logic;
+
+-- Bus
+		romlh_n : in std_logic;
+		ioef_n : in std_logic;
+
+-- Buttons
+		freeze_n : in std_logic;
+
+-- MMC/SPI
+		spi_miso : in std_logic;
+		mmc_cd_n : in std_logic;
+		mmc_wp : in std_logic;
+
+-- MUX CPLD
+		mux_clk : out std_logic;
+		mux : out unsigned(3 downto 0);
+		mux_d : out unsigned(3 downto 0);
+		mux_q : in unsigned(3 downto 0);
+
+-- USART
+		usart_tx : in std_logic;
+		usart_clk : in std_logic;
+		usart_rts : in std_logic;
+		usart_cts : in std_logic;
+
+-- SDRam
+		sd_clk : out std_logic;
+		sd_data : inout std_logic_vector(15 downto 0);
+		sd_addr : out std_logic_vector(12 downto 0);
+		sd_we_n : out std_logic;
+		sd_ras_n : out std_logic;
+		sd_cas_n : out std_logic;
+		sd_ba_0 : out std_logic;
+		sd_ba_1 : out std_logic;
+		sd_ldqm : out std_logic;
+		sd_udqm : out std_logic;
+
+-- Video
+		red : out unsigned(4 downto 0);
+		grn : out unsigned(4 downto 0);
+		blu : out unsigned(4 downto 0);
+		nHSync : buffer std_logic;
+		nVSync : buffer std_logic;
+
+-- Audio
+		sigmaL : out std_logic;
+		sigmaR : out std_logic
+	);
+end entity;
+
+
